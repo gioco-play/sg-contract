1666f193eb4f0ef65244fa1d20362e730c1a41d8
Ellie Su <ellie.su@goldenf.co>
2022-10-14 17:25:00 +0800

    add betLogGrabber, transfer, orderFailCheck

--- src/Contract/SGServiceInterface.php
+++ src/Contract/SGServiceInterface.php
@@ -24,4 +24,53 @@ interface SGServiceInterface {
      * @return mixed
      */
     function gameDetail(string $op_code, string $bet_id, string $language);
+
+    /**
+     * 抓取遊戲紀錄
+     * @param string $op_code
+     * @param integer $past_minutes
+     * @param string $cache_key
+     * @return mixed
+     */
+    function betLogGrabber(string $op_code, int $past_minutes, string $cache_key);
+
+    /**
+     * 全營商 抓取遊戲紀錄
+     * @param integer $past_minutes
+     * @return mixed
+     */
+    function betLogGrabberAll(int $past_minutes);
+
+    /**
+     * 遊戲上分
+     * @param string $op_code
+     * @param string $account
+     * @return mixed
+     */
+    function gameTransferIn(string $op_code, string $account);
+
+    /**
+     * 遊戲下分
+     * @param string $op_code
+     * @param string $account
+     * @param float $amount
+     * @return mixed
+     */
+    function gameTransferOut(string $op_code, string $account, float $amount);
+
+    /**
+     * 取得錢包餘額
+     * @param string $op_code
+     * @param string $account
+     * @return mixed
+     */
+    function getBalance(string $op_code, string $account);
+
+    /**
+     * 上/下分失敗 訂單檢核
+     * @param string $op_code
+     * @param string $order_no
+     * @return mixed
+     */
+    function orderFailCheck(string $op_code, string $order_no);
 }
