33d6f03af69c2d803db51ec43cf7375d8e69b29e
Ellie Su <ellie.su@goldenf.co>
2022-11-09 14:49:10 +0800

    update gameDetail

--- src/Contract/SGServiceInterface.php
+++ src/Contract/SGServiceInterface.php
@@ -19,11 +19,12 @@ interface SGServiceInterface {
     /**
      * 遊戲詳情
      * @param string $op_code
+     * @param string $account
      * @param string $bet_id
      * @param string $language
      * @return mixed
      */
-    function gameDetail(string $op_code, string $bet_id, string $language);
+    function gameDetail(string $op_code, string $account, string $bet_id, string $language);
 
     /**
      * 抓取遊戲紀錄
